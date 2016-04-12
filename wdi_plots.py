b3d7fa871856e4547c851123edf017930b717d7d
gindeleo <schindler3333@gmail.com>
2016-04-12 14:01:26 +0100

    cleanup

--- wdi_plots.py
+++ wdi_plots.py
@@ -48,162 +48,6 @@ def Indicator_finder(str):
 	return pd.read_sql_query("SELECT * FROM Indicators WHERE IndicatorName LIKE @x GROUP BY IndicatorName LIMIT 10",
 		conn,  params={'x': '%'+str +'%'})
 
-def scatter_plot( Indicator_Code_x = 'SP.DYN.IMRT.IN',Indicator_Code_y = 'SH.XPD.PUBL.ZS',Indicator_Code_z='SP.DYN.CBRT.IN',Year_1 = 2010):
-	'''generate scatter plot for choosen year with x and y axis and area of scatter spots as z axis '''
-	
-	#get countries for the selected indicators
-	query_str=str(
-	"SELECT CountryName FROM Indicators WHERE IndicatorCode=:x AND Year=:n "
-	+"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:y AND Year=:n  "
-	+"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:z AND Year=:n "
-	)
-	
-	countries= tuple(pd.read_sql_query( query_str,conn, params={'x': Indicator_Code_x,'y': Indicator_Code_y,'z': Indicator_Code_z, 'n': Year_1}).astype(str).values[:,0])
-
-	
-	# labeling the axis 
-	plt.xlabel(str(Indicator_Name_f(Indicator_Code_x)))
-	plt.ylabel(str(Indicator_Name_f(Indicator_Code_y)))
-
-	# giving the source of the plot marker area in the title. Maybe change to legend
-	plt.title("area=" +str(Indicator_Name_f(Indicator_Code_z)))
-	
-	#normalisation of the plot marker size
-	area_normalisation  = max(pd.read_sql_query("SELECT Value FROM Indicators WHERE  IndicatorCode=:x AND Year=:n AND CountryName IN " +str(countries),
-		conn, params={'x': Indicator_Code_z, 'n': Year_1}).values)#/(len(countries))	
-	# defining x and y values
-	x = pd.read_sql_query("SELECT Value FROM Indicators WHERE  IndicatorCode=:x AND Year=:n AND CountryName IN" +str(countries),
-		conn, params={'x': Indicator_Code_x, 'n': Year_1}).values
-	y = pd.read_sql_query("SELECT Value FROM Indicators WHERE  IndicatorCode=:x AND Year=:n AND CountryName IN" +str(countries),
-		conn, params={'x': Indicator_Code_y, 'n': Year_1}).values
-	# plotmarkersize, it's been normalised, squared and multiplied by 200 to give a visible change in size. This is not necessariliy a good representation
-	area = 200*(pd.read_sql_query("SELECT Value FROM Indicators WHERE  IndicatorCode=:x AND Year=:n AND CountryName IN" +str(countries),
-		conn, params={'x': Indicator_Code_z, 'n': Year_1}).values/area_normalisation)**1.5
-
-	plt.scatter(x, y, s=area, alpha=.7, c = 'red')
-	return plt.show()
-
-def year_data(year_int, df):
-	return pd.concat(
-		[df.loc['x',year_int].set_index('CountryName').rename(columns={"Value": "x"}),
-		df.loc['y',year_int].set_index('CountryName').rename(columns={"Value": "y"}),
-		df.loc['z',year_int].set_index('CountryName').rename(columns={"Value": "z"})], axis = 1).dropna()	
-
-def scatter_plot2(Indicator_Code_x ,Indicator_Code_y ,Indicator_Code_z, Year_1 = 2010,):
-	'''generate scatter plot with bokeh for choosen year with x and y axis and area of scatter spots as z axis '''
-
-	# # drop 'IndicatorName' column
-	# indicator_df.drop(['CountryCode','IndicatorName'], axis=1, inplace=True)
-
-	# set index to 'IndicatorCode'
-
-	indicator_name_df=indicator_df[['IndicatorCode', 'IndicatorName']].set_index('IndicatorName').drop_duplicates('IndicatorCode')
-
-	indicator_df.set_index(['IndicatorCode','Year'], inplace = True)
-	indicator_df.drop(['CountryCode'], axis=1, inplace=True)
-
-	query_str=str(
-	"SELECT CountryName FROM Indicators WHERE IndicatorCode=:x AND Year=:n "
-	+"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:y AND Year=:n  "
-	+"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:z AND Year=:n "
-	)
-
-	countries= tuple(pd.read_sql_query( query_str,conn, params={'x': Indicator_Code_x,'y': Indicator_Code_y,'z': Indicator_Code_z, 'n': Year_1}).astype(str).values[:,0])
-
-	x = indicator_df.loc[Indicator_Code_x,Year_1].Value.values
-	y = indicator_df.loc[Indicator_Code_y,Year_1].Value.values
-	z = indicator_df.loc[Indicator_Code_z,Year_1].Value.values
-
-	z_normalisation = max(z)
-
-	#pack source data
-	source = ColumnDataSource(data=dict(
-	            x=x,
-	            y=y,
-	            countries=countries,
-	            z=.5*z/z_normalisation
-	        )
-	    )
-
-	#create hover tooltips
-	hover = HoverTool(
-	        tooltips=[
-	            ("Country", "@countries"),
-	            ('blas', "@z"),
-	            ("(x,y)", "(@x, @y)"),
-	            ]
-	    )
-
-	#create figure
-	p = Figure(tools=[hover, "pan,box_zoom,reset,resize,save,wheel_zoom"])
-	
-	# set title
-	p.title = (
-		"Year=" +str(Year_1) # a linebreak would be good here, to fit all in the title
-		+ " -- Spot area ~" + indicator_df.loc[Indicator_Code_z].IndicatorName.astype(str).values[0]   )
-	p.title_text_font_size = '16pt'
-	p.title_text_align = 'left' # to ensure the year is displayed even for long names of the z-indicator
-	
-	#set labels
-	p.xaxis.axis_label = indicator_df.loc[Indicator_Code_x].IndicatorName.astype(str).values[0]
-	p.yaxis.axis_label = indicator_df.loc[Indicator_Code_y].IndicatorName.astype(str).values[0]
-	
-	#plot
-	p.scatter('x','y', radius='z', source=source, alpha=.5)
-	
-	# generate selection options for the axis. VERY slow if the set of indicators is too large. For example 'SH' only takes forever.
-	#print Indicator_group('SH.XPD')
-	indicator_options_x = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values)  #default value is 'SH.XPD' for now
-	indicator_options_y = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values)  #default value is 'SP.DYN' for now
-	indicator_options_z = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values) #default value is 'SP.DYN' for now
-
-	# Set up widgets
-	year = Slider(title="Year", value=Year_1, start= 1990 , end=2015, step=1)
-	indicator_x_select = Select(value=Indicator_Name_f(Indicator_Code_x), title='Indicator on x-axis', options=sorted(indicator_options_x))
-	indicator_y_select = Select(value=Indicator_Name_f(Indicator_Code_y), title='Indicator on y-axis', options=sorted(indicator_options_y))
-	indicator_z_select = Select(value=Indicator_Name_f(Indicator_Code_z), title='Indicator as spot area', options=sorted(indicator_options_z))
-	
-	# Set up callbacks
-	def update_title(attrname, old, new):
-		p.title = ("Year=" +str(year.value)
-			+ " -- Spot area ~" + str(indicator_z_select.value)   )
-	
-	def update_xaxis(attrname, old, new):
-		p.xaxis.axis_label = str(indicator_x_select.value)
-
-	def update_yaxis(attrname, old, new):
-		p.yaxis.axis_label = str(indicator_y_select.value)
-	
-	def update_data(attrname, old, new):	
-		x = indicator_df.loc[indicator_name_df.loc[indicator_x_select.value].values.astype(str)[0],year.value].Value.values
-		y = indicator_df.loc[indicator_name_df.loc[indicator_y_select.value].values.astype(str)[0],year.value].Value.values
-		z = indicator_df.loc[indicator_name_df.loc[indicator_z_select.value].values.astype(str)[0],year.value].Value.values
-		z_normalisation = max(z)
-		
-		source.data = dict(
-	        x=x,
-	        y=y,
-	        countries=countries,
-	        z=.5*z/z_normalisation
-	    )
-	
-	#set updates    
-	indicator_x_select.on_change('value', update_data)
-	indicator_x_select.on_change('value', update_xaxis)
-	indicator_y_select.on_change('value', update_data)
-	indicator_y_select.on_change('value', update_yaxis)
-	indicator_z_select.on_change('value', update_data)
-	indicator_z_select.on_change('value', update_title)
-	year.on_change('value', update_title)
-	year.on_change('value', update_data)
-
-
-	# Set up layouts and add to document
-	inputs = VBoxForm(children=[year,indicator_x_select,indicator_y_select,indicator_z_select])
-
-	curdoc().add_root(HBox(children=[inputs, p]))
-
-
 def timeseries_plot(countries_tuple= None, Indicator_Code ='SP.DYN.IMRT.IN'):
 	plt.figure()
 	plt.title(str(Indicator_Name_f(Indicator_Code)))
@@ -215,6 +59,30 @@ def timeseries_plot(countries_tuple= None, Indicator_Code ='SP.DYN.IMRT.IN'):
 			plt.plot(time_and_values(Country_Name,Indicator_Code))
 	#plots for a bunch of countries for just one indicator
 	return plt.show()
+
+# Set up callbacks
+def update_title(attrname, old, new):
+	p.title = ("Year=" +str(year.value)
+		+ " -- Spot area ~" + str(indicator_z_select.value)   )
+
+def update_xaxis(attrname, old, new):
+	p.xaxis.axis_label = str(indicator_x_select.value)
+
+def update_yaxis(attrname, old, new):
+	p.yaxis.axis_label = str(indicator_y_select.value)
+
+def update_data(attrname, old, new):	
+	x = indicator_df.loc[indicator_name_df.loc[indicator_x_select.value].values.astype(str)[0],year.value].Value.values
+	y = indicator_df.loc[indicator_name_df.loc[indicator_y_select.value].values.astype(str)[0],year.value].Value.values
+	z = indicator_df.loc[indicator_name_df.loc[indicator_z_select.value].values.astype(str)[0],year.value].Value.values
+	z_normalisation = max(z)
+	
+	source.data = dict(
+        x=x,
+        y=y,
+        countries=countries,
+        z=.5*z/z_normalisation
+    )
 	
 # ******  Main  ******
 
@@ -231,9 +99,6 @@ else:
 	print "Databade (SQLite) for world indicators not found"
 	sys.exit(0)
 
-#codes = pd.read_sql_query("SELECT IndicatorCode FROM Indicators ",conn).values[:,0]
-#codes_unique = np.unique([str(i[:2]) for i in codes])
-
 #unigue first 2 letters of IndicatorCodes
 codes_unique = np.array(['AG', 'BG', 'BM', 'BN', 'BX', 'CM', 'DC', 'DT', 'EA', 'EG', 'EN',
        'EP', 'ER', 'FB', 'FD', 'FI', 'FM', 'FP', 'FR', 'FS', 'GB', 'GC',
@@ -242,21 +107,103 @@ codes_unique = np.array(['AG', 'BG', 'BM', 'BN', 'BX', 'CM', 'DC', 'DT', 'EA', '
        'TG', 'TM', 'TT', 'TX', 'VC', 'pe'],
       dtype='|S2')
 
-#reduced set for debugging and testing
-indicator_list=('SH.XPD.PUBL.ZS'  ,'SP.DYN.IMRT.IN' ,'SP.DYN.CBRT.IN', 'AG.PRD.CREL.MT', 'BM.GSR.FCTY.CD', 'EN.CO2.TRAN.ZS', 'AG.LND.ARBL.HA', 'DT.DOD.DSTC.ZS', 'EG.ELC.NUCL.ZS', 'EN.CO2.TRAN.ZS')
+#load all data
+default_indicator_group='SH'
+#indicator_all = pd.read_sql_query("SELECT * FROM Indicators",  conn)
+#debugging set
+indicator_all = pd.read_sql_query("SELECT * FROM Indicators WHERE IndicatorCode LIKE @x ", conn,  params={'x': '%'+ default_indicator_group+'%'})
+indicator_df  = indicator_all[indicator_all['IndicatorCode'].str.startswith(default_indicator_group)]
 
-indicator_df = pd.read_sql_query("SELECT * FROM Indicators WHERE IndicatorCode in" +str(indicator_list) ,  conn)
+print "data loaded"
 
-# indicator_df = Indicator_group('SH')
-
-indicator_df = pd.read_sql_query("SELECT * FROM Indicators",  conn)
-
+#setting start values
 Indicator_Code_x = 'SH.XPD.PUBL.ZS'  #Mortality rate, infant (per 1,000 live births)
-Indicator_Code_y = 'SP.DYN.IMRT.IN'	 #Health expenditure, public (% of GDP)
-Indicator_Code_z = 'SP.DYN.CBRT.IN'  #Birth rate, crude (per 1,000 people)
+Indicator_Code_y = 'SH.XPD.PUBL.ZS'	 #Health expenditure, public (% of GDP)
+Indicator_Code_z = 'SH.XPD.PUBL.ZS'  #Birth rate, crude (per 1,000 people)
 
 Year_1=2010
 
-scatter_plot2(Indicator_Code_x ,Indicator_Code_y ,Indicator_Code_z,Year_1)
+indicator_name_df=indicator_df[['IndicatorCode', 'IndicatorName']].set_index('IndicatorName').drop_duplicates('IndicatorCode')
+
+indicator_df.set_index(['IndicatorCode','Year'], inplace = True)
+indicator_df.drop(['CountryCode'], axis=1, inplace=True)
+indicator_df.dropna(inplace=True)
+
+query_str=str(
+"SELECT CountryName FROM Indicators WHERE IndicatorCode=:x AND Year=:n "
++"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:y AND Year=:n  "
++"UNION SELECT CountryName FROM Indicators WHERE IndicatorCode=:z AND Year=:n "
+)
+
+countries= tuple(pd.read_sql_query( query_str,conn, params={'x': Indicator_Code_x,'y': Indicator_Code_y,'z': Indicator_Code_z, 'n': Year_1}).astype(str).values[:,0])
+
+
+x = indicator_df.loc[Indicator_Code_x,Year_1].Value.values
+y = indicator_df.loc[Indicator_Code_y,Year_1].Value.values
+z = indicator_df.loc[Indicator_Code_z,Year_1].Value.values
+
+z_normalisation = max(z)
+
+#pack source data
+source = ColumnDataSource(data=dict(
+            x=x,
+            y=y,
+            countries=countries,
+            z=.5*z/z_normalisation
+        )
+    )
+
+#create hover tooltips
+hover = HoverTool(
+        tooltips=[
+            ("Country", "@countries"),
+            ('blas', "@z"),
+            ("(x,y)", "(@x, @y)"),
+            ]
+    )
+
+#create figure
+p = Figure(tools=[hover, "pan,box_zoom,reset,resize,save,wheel_zoom"])
+
+# set title
+p.title = (
+	"Year=" +str(Year_1) # a linebreak would be good here, to fit all in the title
+	+ " -- Spot area ~" + indicator_df.loc[Indicator_Code_z].IndicatorName.astype(str).values[0]   )
+p.title_text_font_size = '16pt'
+p.title_text_align = 'left' # to ensure the year is displayed even for long names of the z-indicator
+
+#set labels
+p.xaxis.axis_label = indicator_df.loc[Indicator_Code_x].IndicatorName.astype(str).values[0]
+p.yaxis.axis_label = indicator_df.loc[Indicator_Code_y].IndicatorName.astype(str).values[0]
+
+#plot
+p.scatter('x','y', radius='z', source=source, alpha=.5)
+
+# generate selection options for the axis. VERY slow if the set of indicators is too large. For example 'SH' only takes forever.
+indicator_options_x = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values)  #default value is 'SH.XPD' for now
+indicator_options_y = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values)  #default value is 'SP.DYN' for now
+indicator_options_z = tuple(indicator_df.drop_duplicates('IndicatorName').IndicatorName.astype(str).values) #default value is 'SP.DYN' for now
+
+# Set up widgets
+year = Slider(title="Year", value=Year_1, start= 1990 , end=2015, step=1)
+indicator_x_select = Select(value=Indicator_Name_f(Indicator_Code_x), title='Indicator on x-axis', options=sorted(indicator_options_x))
+indicator_y_select = Select(value=Indicator_Name_f(Indicator_Code_y), title='Indicator on y-axis', options=sorted(indicator_options_y))
+indicator_z_select = Select(value=Indicator_Name_f(Indicator_Code_z), title='Indicator as spot area', options=sorted(indicator_options_z))
+
+#set updates    
+indicator_x_select.on_change('value', update_data)
+indicator_x_select.on_change('value', update_xaxis)
+indicator_y_select.on_change('value', update_data)
+indicator_y_select.on_change('value', update_yaxis)
+indicator_z_select.on_change('value', update_data)
+indicator_z_select.on_change('value', update_title)
+year.on_change('value', update_title)
+year.on_change('value', update_data)
+
+
+# Set up layouts and add to document
+inputs = VBoxForm(children=[year,indicator_x_select,indicator_y_select,indicator_z_select])
+
+curdoc().add_root(HBox(children=[inputs, p]))
 
 
